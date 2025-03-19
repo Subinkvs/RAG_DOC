9ad09d46beba73f0852031fe39edbb907d585af1
SubinSundar <subinsundar9624@gmail.com>
2025-03-19 15:29:35 +0530

    minor updates

--- pdfQnA/app.py
+++ pdfQnA/app.py
@@ -15,7 +15,8 @@ from flask_cors import CORS
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+# Enable CORS for all routes
+CORS(app)  
 cache = redis.Redis()
 
 # Retrieve API key
